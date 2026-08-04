82fc0e7427789061d8146a6070a5e6611e2be4b4
JohnIzmir <jrobertsonbfpo6@hotmail.com>
2026-08-04 23:51:47 +0100

    Fix broken gallery images: allow signed storage URLs in next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '*.supabase.co', pathname: '/storage/v1/object/public/**' },
+      { protocol: 'https', hostname: '*.supabase.co', pathname: '/storage/v1/object/sign/**' },
     ],
   },
   async headers() {
